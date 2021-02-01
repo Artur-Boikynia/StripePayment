e27c5239ee8ddd9cddc0d547d5698f633f746880
artur <a.boykinya@gmail.com>
2021-02-01 02:00:57 +0200

    add+pcntl

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<style>
+    html, body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
+    .unit {
+        position: relative;
+        width: 50px;
+        height: 50px;
+        border-radius: 100% ;
+        background-color: green;
+        border: 3px solid black;
+        top: 0;
+        left: 0;
+    }
+</style>
+<body id="wrapper">
+    <div class="unit" id="unit"></div>
+
+    <script src="client/app.js"></script>
+</body>
+</html>
